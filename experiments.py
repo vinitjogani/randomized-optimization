15f389be0e1e69ca0a6b9f818a72234328f5b0a4
Vinit Jogani <vnjogani@gmail.com>
2022-10-14 01:45:24 +0400

    Update annealing readings

--- experiments.py
+++ experiments.py
@@ -31,17 +31,20 @@ def algo_by_params(algo_cls, kwargs, key, values):
     readings = {}
     for prob_cls in PROBLEM_SIZES:
         best_values = []
+        best_evals = []
         best_fitness = []
         for size in PROBLEM_SIZES[prob_cls]:
             prob = prob_cls(size)
             kwargs.update({"problem": prob})
-            f_best, _, value = vary_parameter(algo_cls, kwargs, key, values)
+            f_best, n_evals, value = vary_parameter(algo_cls, kwargs, key, values)
             best_values.append(value)
+            best_evals.append(n_evals)
             best_fitness.append(-f_best)
 
         readings[prob_cls.__name__] = (
             PROBLEM_SIZES[prob_cls],
             best_values,
+            best_evals,
             best_fitness,
         )
     return readings
@@ -51,14 +54,15 @@ def plot_all(readings):
     keys = ["Reverse", "TreasureHunt", "FlipFlop"]
     fig, ax = plt.subplots(
         ncols=3,
-        nrows=2,
-        figsize=(15, 5),
+        nrows=3,
+        figsize=(15, 6),
     )
     for i, k in enumerate(keys):
-        x, y1, y2 = readings[k]
+        x, y1, y2, y3 = readings[k]
         ax[0, i].plot(x, y1)
         ax[1, i].plot(x, y2)
-        ax[1, i].set_xlabel(f"{k}: Problem Size")
+        ax[2, i].plot(x, y3)
+        ax[2, i].set_xlabel(f"{k}: Problem Size")
     return fig, ax
 
 
@@ -72,7 +76,8 @@ def run_experiment(title, ylabel, key, *args, **kwargs):
 
     fig, ax = plot_all(readings)
     ax[0, 0].set_ylabel(ylabel)
-    ax[1, 0].set_ylabel("Fitness (%)")
+    ax[1, 0].set_ylabel("Evaluations")
+    ax[2, 0].set_ylabel("Fitness (%)")
     ax[0, 1].set_title(title)
     fig.tight_layout()
     fig.savefig(path + ".png")
@@ -106,7 +111,7 @@ if __name__ == "__main__":
         "Optimal temperature",
         "annealing_temperature_by_size",
         Annealing,
-        {"decay": 0.99, "min": 0.01, "max_attempts": 25},
+        {"decay": 0.99, "min": 0.01, "max_attempts": 30},
         "T",
         values=[0.25, 0.5, 1, 1.5, 2, 3, 4],
     )
